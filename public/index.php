b6345afd98f92cefd65bd2607944964bd5a55afc
nad-trssv <alienlamahope@gmail.com>
2022-09-04 16:04:53 +0300

    Lesson 7 | Classwork tasks

--- public/index.php
+++ public/index.php
@@ -1,15 +1,21 @@
 <?php
 
-use App\Strategy\Handlers\SortHandler;
-use App\Strategy\Enum\PaymentType;
+// use App\Strategy\Handlers\SortHandler;
+// use App\Strategy\Enum\PaymentType;
+
+use App\Lesson7\Classwork;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$pay = new SortHandler();
+// $pay = new SortHandler();
 
-$pay->handle(PaymentType::QIWI_PAYMENT , '56$', '37255944520 ');
-$pay->handle(PaymentType::YANDEX_PAYMENT , '72.90$', '37255855025 ');
-$pay->handle(PaymentType::WEBMONEY_PAYMENT, '13.50$', '37255941000 ');
+// $pay->handle(PaymentType::QIWI_PAYMENT , '56$', '37255944520 ');
+// $pay->handle(PaymentType::YANDEX_PAYMENT , '72.90$', '37255855025 ');
+// $pay->handle(PaymentType::WEBMONEY_PAYMENT, '13.50$', '37255941000 ');
+
+$cw7 = new Classwork();
+$cw7->ulesandeid();
+
 
 
 
